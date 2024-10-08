2e216a22c14cb74e3b916c923bdc3c8622d18359
evgeniy <evgeniy3234@gmail.com>
2024-10-08 23:02:44 +0300

    fix

--- src/Carusel/Carusel.jsx
+++ src/Carusel/Carusel.jsx
@@ -1,23 +1,31 @@
 import Carousel from 'react-bootstrap/Carousel';
-import Source1 from "../ImageSource/CaruselSource1.png"
+import CaruselSource1 from "../ImageSource/CaruselSource1.jpg"
+import CaruselSource2 from "../ImageSource/CaruselSource2.jpg"
+import CaruselSource3 from "../ImageSource/CaruselSource3.jpg"
+import CaruselSource4 from "../ImageSource/CaruselSource4.jpg"
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Carusel.css"
 function IndividualIntervalsExample() {
   return (
     <div className='Caruselcontainer'>
-        <Carousel>
-        <Carousel.Item interval={2000}>
-            <img src={Source1} className="d-block w-100" alt=""/>
+        <Carousel style={{height : "750px"}}>
+        <Carousel.Item interval={2000} style={{height : "750px"}}>
+            <img src={CaruselSource1} className="d-block w-100 mh-100" alt=""/>
             <Carousel.Caption>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={2000}>
-            <img src={Source1} className="d-block w-100" alt=""/>
+        <Carousel.Item interval={2000} style={{height : "750px"}}>
+            <img src={CaruselSource2} className="d-block w-100 mh-100" alt=""/>
             <Carousel.Caption>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={2000}>
-            <img src={Source1} className="d-block w-100" alt=""/>
+        <Carousel.Item interval={2000} style={{height : "750px"}}>
+            <img src={CaruselSource3} className="d-block w-100 mh-100" alt=""/>
+            <Carousel.Caption>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000} style={{height : "750px"}}>
+            <img src={CaruselSource4} className="d-block w-100 mh-100" alt=""/>
             <Carousel.Caption>
             </Carousel.Caption>
         </Carousel.Item>
